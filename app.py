0a9ef3057eba809f3d3e0260a1c6b2fd26042a4b
34412as@gmail.com <34412as@gmail.com>
2025-02-16 02:41:08 +0200

    שם הקובץ שיורד הוא שם המקורי

--- app.py
+++ app.py
@@ -11,42 +11,12 @@ def download_youtube_video(url, output_path):
         'nocheckcertificate': True
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        info_dict = ydl.extract_info(url, download=True)
+        video_title = info_dict.get('title', 'video')  # שם הסרטון
+        video_ext = info_dict.get('ext', '')       # סיומת הסרטון
+        return f"{video_title}.{video_ext}"
 
 
-#עם כניסה לעומק
-# def get_video_list(channel_url):
-#     options = {
-#         'extract_flat': False,  # מאפשר כניסה לעומק עבור קטגוריות
-#         'quiet': True,
-#         'skip_download': True,
-#         'nocheckcertificate': True
-#     }
-#     with yt_dlp.YoutubeDL(options) as ydl:
-#         info = ydl.extract_info(channel_url, download=False)
-    
-#     video_list = []
-#     for entry in info.get('entries', []):
-#         # בדיקה אם יש תת-ערכים בקטגוריה הנוכחית
-#         if entry.get('_type') == 'playlist':
-#             # כניסה לרשימת הסרטונים בקטגוריה
-#             for video in entry.get('entries', []):
-#                 if video.get('id'):  # ודא שהסרטון מכיל מזהה
-#                     video_list.append({
-#                         'title': video.get('title'),
-#                         'url': f"https://www.youtube.com/watch?v={video.get('id')}",
-#                         'upload_date': video.get('upload_date')
-#                     })
-#         elif entry.get('_type') == 'video':  # אם זה סרטון בודד
-#             video_list.append({
-#                 'title': entry.get('title'),
-#                 'url': f"https://www.youtube.com/watch?v={entry.get('id')}",
-#                 'upload_date': entry.get('upload_date')
-#             })
-    
-#     return video_list
-
-#בלי כניסה לעומק
 def get_video_list(channel_url):
     options = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36',
@@ -77,8 +47,8 @@ def index():
 def download():
     video_url = request.form['url']
     output_file = mktemp(suffix=".mp4")
-    download_youtube_video(video_url, output_file)
-    return send_file(output_file, as_attachment=True, download_name="downloaded_video.mp4")
+    video_name = download_youtube_video(video_url, output_file)
+    return send_file(output_file, as_attachment=True, download_name=video_name)
 
 @app.route('/channel_videos', methods=['POST'])
 def channel_videos():
